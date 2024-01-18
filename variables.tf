698dad9254d8ec4cebce28187916b4da2c157952
aaidapa96 <96769025+aaidapa96@users.noreply.github.com>
2024-01-18 14:49:43 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "resource_group_name" {
   description = "Name of the Azure Resource Group"
   type        = string
+  default     = "aaida_rg"
 }
 
 variable "location" {
   description = "Azure region where the Resource Group will be created"
   type        = string
+  default     = "centralindia"
 }
